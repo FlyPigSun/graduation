748d16f484cb01babd5731876eaad8f97efb6efb
unknown <Administrator@ZGC-20130411WNT.(none)>
2013-11-18 10:34:36 +0800

    1

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -20,7 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('/applicaton/resources/views/login.html');
+		$this->load->view('login.html');
 	}
     //登录
    
