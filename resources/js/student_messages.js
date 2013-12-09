0331c2fadefec46c806f51f3b0e092ed72e1f2ef
Sun Ji <happy496@gmail.com>
2013-12-09 12:42:22 +0800

    message

--- resources/js/student_messages.js
+++ resources/js/student_messages.js
@@ -6,6 +6,8 @@ activityCircle.student.messages = {
 	initialize : function(){
 		var me = this;
 		$('.activitycircle-messages-mode-btn').on('click',me.changeTab);
+		$(document).delegate('.activitycircle-messages-receivemessage-table tr','click',{'item':me},me.showMessageDetail);
+		this.getReceiveMessages();
 	},
 	changeTab : function(){
 		$('.activitycircle-messages-mode-btn').siblings('.active').removeClass('active');
@@ -13,12 +15,14 @@ activityCircle.student.messages = {
         var btn = $.trim($(this).html());
         switch(btn){
             case '收件箱' :
-                $('.yike-studentcenter-receivebox-area').show();
-                $('.yike-studentcenter-receivebox-area').siblings().hide();
+                $('.activitycircle-messages-receivebox-area').show();
+                $('.activitycircle-messages-receivebox-area').siblings().hide();
+                activityCircle.student.messages.getReceiveMessages();
                 break;
             case '发件箱' :
-                $('.yike-studentcenter-sendmessage-area').show();
-                $('.yike-studentcenter-sendmessage-area').siblings().hide();
+                $('.activitycircle-messages-sendmessage-area').show();
+                $('.activitycircle-messages-sendmessage-area').siblings().hide();
+                activityCircle.student.messages.getSendMessages();
                 break;
         }
 	},
@@ -31,13 +35,44 @@ activityCircle.student.messages = {
                 'CONTENT-TYPE': 'application/x-www-form-urlencoded'
             },
             success : function(responseText){
-                $('.yike-studentcenter-record-table tbody').html('');
+                $('.activitycircle-messages-receivemessage-table tbody').html('');
                 var res = responseText;
                 res = $.parseJSON(res);
                 var data = res.data;
                 $.each(data,function(key,item){
+                	var tpl = $('#activitycircle-messages-receivemessage-table-template').html();
+                    var htmlStr = Mustache.to_html(tpl, item).replace(/^\s*/mg, '');
+                    $('.activitycircle-messages-receivemessage-table tbody').append(htmlStr);
                 });
+                $('.activitycircle-messages-receivemessage-table tr:odd').addClass('odd');
+                $('.activitycircle-messages-receivemessage-table tr:even').addClass('even');
             }
         });
+	},
+	getSendMessages : function (){
+		$('.activitycircle-messages-sendmessage-table tbody').html('');
+		$.ajax({
+            url : '/student/outBox',
+            type : 'get',
+            headers:{
+                'CONTENT-TYPE': 'application/x-www-form-urlencoded'
+            },
+            success : function(responseText){
+                $('.activitycircle-messages-sendmessage-table tbody').html('');
+                var res = responseText;
+                res = $.parseJSON(res);
+                var data = res.data;
+                $.each(data,function(key,item){
+                	var tpl = $('#activitycircle-messages-sendmessage-table-template').html();
+                    var htmlStr = Mustache.to_html(tpl, item).replace(/^\s*/mg, '');
+                    $('.activitycircle-messages-sendmessage-table tbody').append(htmlStr);
+                });
+                $('.activitycircle-messages-sendmessage-table tr:odd').addClass('odd');
+                $('.activitycircle-messages-sendmessage-table tr:even').addClass('even');
+            }
+        });
+	},
+	showMessageDetail : function(){
+
 	}
 }
