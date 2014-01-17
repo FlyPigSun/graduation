5b0c3058dd957a305a6eb223534d7002a732f295
Sun Ji <happy496@gmail.com>
2014-01-17 14:45:46 +0800

    recommend

---
+++ resources/js/student_teacher_recommend.js
@@ -0,0 +1,42 @@
+/**
+  *学生：教师推荐活动页面
+  *author: 孙骥
+ **/
+activityCircle.student.teacherRecommend = {
+	initialize : function(){
+		activityCircle.student.teacherRecommend.refresh();
+	},
+    refresh : function(){
+        $('.student-teacher-recommend-undone-center').html('');
+        $('.student-teacher-recommend-done-center').html('');
+        $.ajax({
+            url : '/student/myActivity',
+            type : 'post',
+            headers:{
+                'CONTENT-TYPE': 'application/x-www-form-urlencoded'
+            },
+            success : function(responseText){
+                var res = responseText;
+                res = $.parseJSON(res);
+                var data = res.data;
+                $.each(data,function(key,item){
+                    var tpl = $('#student-teacher-recommend-activity-template').html();
+                    var htmlStr = Mustache.to_html(tpl, item).replace(/^\s*/mg, '');
+                    if(item.is_finish == 0)
+                        $('.student-teacher-recommend-undone-center').append(htmlStr);
+                    else
+                        $('.student-teacher-recommend-done-center').append(htmlStr);
+                    $('.student-teacher-recommend-activity-level-'+item.id).raty({
+                        readOnly : true,
+                        score : item.level,
+                        number : 3,
+                        hints : ['中等', '提高', '竞赛'],
+                        noRatedMsg: '活动难度'
+                    });
+                });
+                $('.student-teacher-recommend-undone-center').append('<div class="clear"></div>');
+                $('.student-teacher-recommend-done-center').append('<div class="clear"></div>');
+            }
+        });
+    }
+}
